60af27a42cbc10396ca1ca289e8432fbf6a8f9d8
fadwa bouabi <fadwabouabi@gmail.com>
2019-01-05 17:48:50 +0100

    ajout hh

--- testgit/src/testgit/testClass.java
+++ testgit/src/testgit/testClass.java
@@ -5,12 +5,12 @@ public class testClass {
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-
 		int c=0;
 		int b=20;
 		int gh=30;
 		double ggg=20;
 		String d="fgfg";
+		int hh=65;
 		return super.toString();
 	}
 	
